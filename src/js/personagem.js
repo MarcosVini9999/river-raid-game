a93ca1a71dfe8539d6a1a9b8518bdb4b1b21e785
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2022-11-26 16:29:37 -0300

    feat: player controller

---
+++ src/js/personagem.js
@@ -0,0 +1,20 @@
+const personagem = document.getElementById("personagem");
+let distancia = 45;
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+  e = e || window.event;
+
+  if (e.keyCode == "37") {
+    if (distancia < 90) distancia += 1;
+    personagem.style.right = `${distancia}%`;
+    console.log(distancia);
+    // left arrow
+  } else if (e.keyCode == "39") {
+    if (distancia < 100 && distancia > 0) distancia -= 1;
+    personagem.style.right = `${distancia}%`;
+    console.log(distancia);
+    // right arrow
+  }
+}
